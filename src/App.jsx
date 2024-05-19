29256fee4800b89f692136289e6a903bf9d08dac
Sedat Polat <sedatpolat355@gmail.com>
2024-05-19 13:17:03 +0300

    TAILWINDCSS YÜKLENDİ

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './assets/css/style.css'
 function App() {
   return(
     <div>
-      <h1>hello</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
